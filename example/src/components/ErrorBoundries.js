0bb513f52f752b4305d55b69b6f3895c45c94054
lijinfei <lijinfei@devcloud.com>
2021-05-24 18:28:47 +0800

    ErrorBoundries

---
+++ example/src/components/ErrorBoundries.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class ErrorBoundries extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {hasError: false}
+    }
+    static getDerivedStateFromError(error) {
+        console.log('error', error)
+        return {hasError: true}
+    }
+    componentDidCatch(error, errorInfo) {
+
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
