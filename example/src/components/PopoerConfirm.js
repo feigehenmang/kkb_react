f0e11e528a260d7b1b798346d2ee0d2274f40fdb
lijinfei <lijinfei@devcloud.com>
2021-05-28 17:01:44 +0800

    hooks poperConfirm

--- example/src/components/PopoerConfirm.js
+++ example/src/components/PopoerConfirm.js
@@ -1,41 +1,66 @@
 import Popper from 'popper.js'
-import React, { Component } from 'react'
+import React, { Component, useEffect, useLayoutEffect, useRef } from 'react'
 import ClickOutSide from '../utils/clickOutSide'
 import './popover.css'
 
-export default class PopoerConfirm extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            show: false
-        }
-        this.reference = React.createRef();
-        this.popper = React.createRef();
-    }
+// export default class PopoerConfirm extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.reference = React.createRef();
+//         this.popper = React.createRef();
+//     }
     
-    componentDidUpdate() {
-        this.popperJS = new Popper(this.reference.current, this.popper.current, {
+//     componentDidUpdate() {
+//         this.popperJS = new Popper(this.reference.current, this.popper.current, {
+//             placement: 'top-end',
+//             modifiers: {
+//               computeStyle: {
+//                 gpuAcceleration: false
+//               }
+//             }
+//           });
+//     }
+    
+//     componentWillUnmount() {
+//         this.popperJS.destroy()
+//     }
+//     render() {
+//         const {visible, content, children, hide} = this.props;
+//         const newChildren = React.cloneElement(children, { ref: this.reference, className: 'popover-container'})
+//         return (
+//             <ClickOutSide clickOutSideFn={hide}>
+//                 {newChildren}
+//                 <div ref={this.popper} className="popover" style={{display: visible ? 'block':'none'}}>{content}</div>
+//             </ClickOutSide>
+            
+//         )
+//     }
+// }
+
+export default function PopoerConfirm({children, hide, visible, content}) {
+    const popper = useRef();
+    const reference = useRef();
+    const popperJs = useRef();
+    const newChildren = React.cloneElement(children, { ref: (el) => {
+        reference.current = el;
+    }, className: 'popover-container'})
+    useLayoutEffect(() => {
+        popperJs.current = new Popper(reference.current, popper.current, {
             placement: 'top-end',
             modifiers: {
               computeStyle: {
                 gpuAcceleration: false
               }
             }
-          });
-    }
-    
-    componentWillUnmount() {
-        this.popperJS.destroy()
-    }
-    render() {
-        const {visible, content, children, hide} = this.props;
-        const newChildren = React.cloneElement(children, { ref: this.reference, className: 'popover-container'})
-        return (
-            <ClickOutSide clickOutSideFn={hide}>
-                {newChildren}
-                <div ref={this.popper} className="popover" style={{display: visible ? 'block':'none'}}>{content}</div>
-            </ClickOutSide>
-            
-        )
-    }
+        });
+        return () => {
+            popperJs.current.destroy();
+        }
+    })
+    return (
+        <ClickOutSide clickOutSideFn={hide}>
+            {newChildren}
+            <div ref={popper} className="popover" style={{display: visible ? 'block':'none'}}>{content}</div>
+        </ClickOutSide>
+    )
 }
